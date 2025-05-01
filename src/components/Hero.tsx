09fe66a8488eb232dd603065e769ab88093c2689
tpixl <stephane.paquier@gmail.com>
2025-05-01 23:15:28 +0200

    testok

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC = () => {
             2012 <span className="text-dodge-yellow">Dodge Challenger</span> SRT8 Yellow Jacket
           </h2>
           <p className="reveal text-xl md:text-2xl text-dodge-gray-light mb-8">
-            Transmission Manuelles | 470 CV | Moteur HEMI V8 6.4L
+            Transmission Manuelle | 470 CV | Moteur HEMI V8 6.4L
           </p>
           <div className="reveal flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
             <a href="#caractéristiques" className="inline-block bg-dodge-yellow text-dodge-black py-3 px-8 rounded-md font-bold text-lg hover:bg-dodge-yellow-light transition duration-300">
