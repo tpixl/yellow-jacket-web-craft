48790815c2fbb2b67c0d11443a992fdbac423f87
tpixl <stephane.paquier@gmail.com>
2025-05-07 14:18:58 +0200

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed top-0 left-0 w-full bg-dodge-black bg-opacity-80 backdrop-blur-md z-50">
+    <header className="fixed top-0 left-0 w-1500 bg-dodge-black bg-opacity-80 backdrop-blur-md z-50">
       <div className="container mx-auto py-4 px-4 flex justify-between items-center">
         <h1 className="text-2xl font-bold text-dodge-yellow">
           <span className="text-white">SRT8</span> Yellow Jacket
