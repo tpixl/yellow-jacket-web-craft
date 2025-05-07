8dbd57a229f69edcb9ca2f8b0bf1a4c87aecbf84
tpixl <stephane.paquier@gmail.com>
2025-05-07 17:35:40 +0200

    testP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-dodge-black bg-opacity-80 backdrop-blur-md z-50">
-    
+     
     </header>
   );
 };
