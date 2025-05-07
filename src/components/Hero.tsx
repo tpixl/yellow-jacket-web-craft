490039d1f97b950771f4a676713b260270151799
tpixl <stephane.paquier@gmail.com>
2025-05-07 14:07:48 +0200

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return <section id="aperçu" className="relative h-screen flex items-center justify-center overflow-hidden">
       <div className="absolute inset-0 bg-dodge-black">
         <div className="absolute inset-0 bg-gradient-to-b from-dodge-black via-transparent to-dodge-black opacity-80"></div>
-        <div className={`absolute inset-0 bg-no-repeat ${isMobile ? "bg-[url('https://i.ibb.co/W47WGKcC/20250501-224529.jpg')] bg-cover bg-center" : "bg-[url('https://i.ibb.co/B2HmsGyK/20240417-203841-HALF.jpg')] bg-contain bg-center"}`} style={{
+        <div className={`absolute inset-0 bg-no-repeat ${isMobile ? /*si mobile :*/"bg-[url('https://i.ibb.co/B2HmsGyK/20240417-203841-HALF.jpg')] bg-cover bg-center" : /*si no mobile*/"bg-[url('https://i.ibb.co/B2HmsGyK/20240417-203841-HALF.jpg')] bg-contain bg-center"}`} style={{
         width: "1500px",
         /*height: "2000px",*/
         maxWidth: "100%",
