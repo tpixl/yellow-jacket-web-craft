c7b7db5670ad39e6f04c2950c01b280c57801d32
tpixl <stephane.paquier@gmail.com>
2025-05-07 15:18:38 +0200

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,9 +12,10 @@ const Hero: React.FC = () => {
         margin: "0 auto"
       }}></div>
       </div>
-      <div className="container z-10 mx-0 my-0 py-0 px-0 width-1500" style={{
+      <div className="container z-10 mx-0 my-0 py-0 px-0" style={{
         display: "flex",
         justifyContent: 'center',
+        width: "1500"
       }}>
         <div className="max-w-3xl py-0 px-[17px]">
           <h2 className="reveal text-5xl md:text-7xl font-bold text-white leading-tight mb-4">
