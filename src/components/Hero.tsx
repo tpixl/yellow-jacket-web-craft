6015f65b3c7fe424f0852ca81387a6e97f9d062b
tpixl <stephane.paquier@gmail.com>
2025-05-07 12:49:29 +0200

    test continue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,13 +22,13 @@ const Hero: React.FC = () => {
           <div className="reveal flex flex-col space-y-4">
             <a href="#caractéristiques" className="inline-block bg-dodge-yellow text-dodge-black py-3 rounded-md font-bold text-lg hover:bg-dodge-yellow-light transition duration-300" style={{
               width: "250px",
-              margin: "20px"
+              margin: "20px auto"
             }}>
               Voir les Caractéristiques
             </a>
             <a href="#galerie" className="inline-block bg-transparent border-2 border-white text-white py-3 rounded-md font-bold text-lg hover:bg-white hover:bg-opacity-10 transition duration-300" style={{
               width: "250px",
-              margin: "20px"
+              margin: "20px auto"
             }}>
               Voir la Galerie
             </a>
