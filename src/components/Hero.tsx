67f1e6d5aac4cd763c73781c9cdd876b5b9df60b
tpixl <stephane.paquier@gmail.com>
2025-05-12 14:14:26 +0200

    U

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,13 +28,13 @@ const Hero: React.FC = () => {
             Transmission Manuelle | 470 CV | Moteur HEMI V8 6.4L
           </p>
           <div className="reveal flex flex-col space-y-4">
-            {/*<a href="#caractéristiques" className="inline-block bg-dodge-yellow text-dodge-black py-3 rounded-md font-bold text-lg hover:bg-dodge-yellow-light transition duration-300 px-[10px]" style={{
+            <a href="#caractéristiques" className="inline-block bg-dodge-yellow text-dodge-black py-3 rounded-md font-bold text-lg hover:bg-dodge-yellow-light transition duration-300 px-[10px]" style={{
               width: "250px",
               maxWidth: "100%"
             }}>
               Voir les Caractéristiques
             </a>
-            <a href="#galerie" className="inline-block bg-transparent border-2 border-white text-white py-3 rounded-md font-bold text-lg hover:bg-white hover:bg-opacity-10 transition duration-300 px-[10px]" style={{
+            {/*<a href="#galerie" className="inline-block bg-transparent border-2 border-white text-white py-3 rounded-md font-bold text-lg hover:bg-white hover:bg-opacity-10 transition duration-300 px-[10px]" style={{
               width: "250px",
               maxWidth: "100%"
             }}>
