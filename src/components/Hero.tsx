dce16eb999b6ca73386949f6c789aad944c838c6
tpixl <stephane.paquier@gmail.com>
2025-05-12 12:59:04 +0200

    W

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero: React.FC = () => {
         display: "flex",
         /*flexDirection: "row",*/
         justifyContent: "center",
-        alignItems: "bottom",
+        alignItems: "left",
         height: "800px",
         maxWidth: "1100px",
       }}>
