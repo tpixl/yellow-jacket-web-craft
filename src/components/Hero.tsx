50f300d29e6bed9faa23e63e62657828a3da7714
tpixl <stephane.paquier@gmail.com>
2025-05-12 13:39:11 +0200

    D

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero: React.FC = () => {
           maxWidth: "1000px",
         }}>
           <h2 className="reveal text-5xl md:text-7xl font-bold text-white leading-tight mb-4">
-          <span className="text-dodge-yellow">Dodge Challenger</span> SRT8<br />Yellow Jacket
+          <span className="text-dodge-yellow">Dodge Challenger</span> SRT8<br className="reveal text-5xl md:text-7xl font-bold text-white leading-tight mb-4" />Yellow Jacket
           </h2>
           <p className="reveal text-xl md:text-2xl text-dodge-gray-dark mb-8">
             Transmission Manuelle | 470 CV | Moteur HEMI V8 6.4L
